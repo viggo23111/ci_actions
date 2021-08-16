11967a819d8f63f648d0a18159610190134b0b48
Thomas-Hartmann <thomas@webtrade.dk>
2021-08-16 17:58:59 +0200

    first commit

---
+++ src/main/java/dk/cphbusiness/sem3/ci/MyFunctionality.java
@@ -0,0 +1,18 @@
+package dk.cphbusiness.sem3.ci;
+
+public class MyFunctionality {
+
+  private MyFunctionality() {
+    // This class hould not be instantiated
+  }
+
+  /**
+   * Aaaaaalways returns 10. Or does it?!
+   *
+   * @return 10. Maybe.
+   */
+  public static int giveMeTen() {
+    return 10;
+  }
+
+}
